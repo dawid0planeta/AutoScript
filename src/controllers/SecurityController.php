e2d5d16ab2bab03aca24fc9edc9619e6daa298c2
Dawid Płaneta <dawid0planeta@gmail.com>
2022-01-27 17:02:26 +0100

    Add simple database integration

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,16 +2,36 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
 
     public function login() {
+        $userRepository = new UserRepository();
+
         if (!$this->isPost()) {
             return $this->render('login');
         }
-        $user = new User('ab@gmail.com', 'admin', 'John', 'Snow');
-        var_dump($_POST);
-        die();
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $user = $userRepository->getUser($email);
+
+        if (!$user) {
+            return $this->render('login', ['messages' => ["User not found"]]);
+        }
+
+        if ($user->getEmail() !== $email) {
+            return $this->render('login', ['messages' => ["User with this email not found"]]);
+        }
+
+        if ($user->getPassword() !== $password) {
+            return $this->render('login', ['messages' => ["Wrong password"]]);
+        }
+
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: $url/my_snippets");
     }
 
     public function register() {
